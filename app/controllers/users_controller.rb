9210225b6a795fc677079eb60ab83b53e6771451
shota kikuchi <shota3557@gmail.com>
2022-10-09 23:32:02 +0900

    showアクション実施のため

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new  
+    end
+  end
+
+  def update
+   
+  end
+
+  
+  def destroy
+    
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
