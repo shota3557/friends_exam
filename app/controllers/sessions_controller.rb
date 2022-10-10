8c9e32f1269fd56ce045ea3a1bac07d31a539039
shota kikuchi <shota3557@gmail.com>
2022-10-10 12:37:07 +0900

    skip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   def new
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 
   def create
